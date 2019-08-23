5cf4d0c21ff6c6107a7f88943e83d9c890cfafdd
RMuskovets <rmuskovets@gmail.com>
2019-08-23 15:46:22 +0300

    minified eeprom.lua, 4xx -> 363 bytes

--- eeprom.lua
+++ eeprom.lua
@@ -1,16 +1,1 @@
-local repo,file="RMuskovets/OCOS","installer/main.lua"
-require("component").eeprom.set([[
-    local handle, data, chunk = component.proxy(component.list("internet")()).request("https://raw.githubusercontent.com/]]..repo..[[/master/]]..file..[["), ""
-   
-    while true do
-        chunk = handle.read(math.huge)
-        if chunk then
-            data = data .. chunk
-        else
-            break
-        end
-    end
- 
-    handle.close()
-    load(data)()
-]])
+local repo,file="RMuskovets/OCOS","installer/main.lua" require("component").eeprom.set([[local handle,data,chunk=component.proxy(component.list("internet")()).request("https://raw.githubusercontent.com/]]..repo..[[/master/]]..file..[["),"" while true do chunk=handle.read(math.huge) if chunk then data=data..chunk else break end end handle.close() load(data)()]])
